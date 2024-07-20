0f253be6736d88188146aa39119def148ffcbbfd
Hyunah Lee <128590006+gusdk19@users.noreply.github.com>
2024-07-20 16:33:50 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,9 +1,9 @@
 def solution(price):
     if price >= 500000:
-        return int(price * 80 / 100 )
+        return int(price * 0.8 )
     elif price >= 300000:
-        return int(price * 90/ 100 )
+        return int(price * 0.9 )
     elif price >= 100000:
-        return int(price * 95 / 100 )
+        return int(price * 0.95 )
     else:
         return price
