9d0e616793937e3619a92c72f54e052e86956772
Hyunah Lee <128590006+gusdk19@users.noreply.github.com>
2024-07-23 09:40:32 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,3 +1,10 @@
 import math
 def solution(num_list):
-    return sum(num_list) if len(num_list) >= 11 else math.prod(num_list)
+    result = 1
+    # return sum(num_list) if len(num_list) >= 11 else math.prod(num_list)
+    if len(num_list) >= 11:
+        return sum(num_list)
+    else:
+        for i in num_list:
+            result *= i
+        return result
