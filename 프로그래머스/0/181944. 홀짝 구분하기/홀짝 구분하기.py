1c01efd56c3e702f0d7a64b891fe25b691d242c6
Hyunah Lee <128590006+gusdk19@users.noreply.github.com>
2024-07-26 19:06:19 +0900

    [level 0] Title: 홀짝 구분하기, Time: 12.20 ms, Memory: 7.54 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.py
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.py
@@ -1,2 +1,3 @@
 a = int(input())
-print(str(a) + ' is odd' if a%2 else str(a) + ' is even')
+# print(str(a) + ' is odd' if a%2 else str(a) + ' is even')
+print(f'{a} is odd' if a%2 else f'{a} is even')
