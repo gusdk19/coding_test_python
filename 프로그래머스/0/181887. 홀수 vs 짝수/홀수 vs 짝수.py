1c52e0dae1512a022874ea315ca772b5ba63f25b
Hyunah Lee <128590006+gusdk19@users.noreply.github.com>
2024-07-28 15:32:00 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,4 +1,6 @@
 def solution(num_list):
-    odd = sum([num for i, num in enumerate(num_list) if i%2==1])
-    even = sum([num for i, num in enumerate(num_list) if i%2 == 0])
+    # odd = sum([num for i, num in enumerate(num_list) if i%2==1])
+    # even = sum([num for i, num in enumerate(num_list) if i%2 == 0])
+    odd = sum(num_list[::2])
+    even = sum(num_list[1::2])
     return max(odd, even)
