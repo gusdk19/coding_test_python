8564c6191cc0a56eba33d543af70086f9617eceb
Hyunah Lee <128590006+gusdk19@users.noreply.github.com>
2024-07-28 15:17:54 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,10 +1,13 @@
 def solution(my_string, num1, num2):
-    str = ''
-    for i in range(len(my_string)):
-        if i == num1:
-            str += my_string[num2]
-        elif i == num2:
-            str += my_string[num1]
-        else:
-            str += my_string[i]
-    return str
+    # str = ''
+    # for i in range(len(my_string)):
+    #     if i == num1:
+    #         str += my_string[num2]
+    #     elif i == num2:
+    #         str += my_string[num1]
+    #     else:
+    #         str += my_string[i]
+    # return str
+    list_str = list(my_string)
+    list_str[num1], list_str[num2] = list_str[num2], list_str[num1]
+    return ''.join(list_str)
