da6602ef20b08675bf2324021f5c2b4812ad38cf
Hyunah Lee <128590006+gusdk19@users.noreply.github.com>
2024-07-31 18:56:25 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
@@ -1,6 +1,7 @@
 def solution(n):
-    result = 0
-    n_list = [i for i in str(n)]
-    for j in n_list:
-        result += int(j)
-    return result
+    # result = 0
+    # n_list = [i for i in str(n)]
+    # for j in n_list:
+    #     result += int(j)
+    # return result
+    return sum([int(i) for i in str(n)])
