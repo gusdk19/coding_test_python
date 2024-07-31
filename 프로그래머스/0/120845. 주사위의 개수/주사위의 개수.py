38abf238f43fe4faea990c12a559f049143b83a3
Hyunah Lee <128590006+gusdk19@users.noreply.github.com>
2024-07-31 18:29:04 +0900

    [level 0] Title: 주사위의 개수, Time: 0.00 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.py
@@ -1,8 +1,7 @@
 def solution(box, n):
     result = 1
-    max_num = [i // n for i in box]
-    for i in max_num:
-        result *= i
+    for i in box:
+        result *= i // n
     return result
 
 # (가로 // n) * (세로 // n) * (높이 // n)
